082db2b67529cd2f9eb43c092147771977635e82
Abadabada <mhadden-a@hotmail.com>
2021-06-27 11:19:54 +0200

    Added the logic behind display for the pirate - no on click yet

---
+++ Assets/Scripts/UI Elements scripts/DisplayTraits.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class DisplayTraits : MonoBehaviour
+{
+
+    public GameObject PirateTaskNames;
+    public GameObject PirateModefiers;
+    private List<Text> pirateTaskNames;
+    private List<Text> pirateModefiers;
+    public GameObject Pirate;
+    private List<Trait> traits;
+    private string[] taskNames = new string[3];
+    private float[] modifiers = new float[3];
+
+    // Start is called before the first frame update
+    void Start() 
+    {   
+        traits = Pirate.GetComponents<Trait>().ToList();
+        pirateTaskNames = PirateTaskNames.GetComponents<Text>().ToList();
+        pirateModefiers = PirateModefiers.GetComponents<Text>().ToList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        getPirateTraits();
+        showPirateTaskNames();
+        showPirateModefiers();
+    }
+
+    void getPirateTraits(){
+        int index = 0;
+        foreach(Trait trait in traits){
+            taskNames[index] = trait.taskEffects[0].taskName;
+            modifiers[index] = trait.taskEffects[0].modifier;
+            index ++;
+        }
+    }
+
+    void showPirateTaskNames(){
+        int index = 0;
+        foreach (Text pirateTaskName in pirateTaskNames)
+        {
+            pirateTaskName.text = taskNames[index];
+            index ++;
+        }
+    }
+
+    void showPirateModefiers(){
+        int index = 0;
+        foreach (Text pirateModefier in pirateModefiers)
+        {
+            pirateModefier.text = string.Format("{0:N0}", modifiers[index]);
+            index ++;
+        }
+    }
+
+    
+}
